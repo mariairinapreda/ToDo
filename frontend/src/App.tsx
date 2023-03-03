78984579a633e87aec6f9ccae8994b2954a79c4c
mariairinapreda <mariairina.predaeu@gmail.com>
2023-03-03 17:45:23 +0200

    implemented routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/header";
+import TaskPage from "./pages/taskPage/taskPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Form from "./components/form/form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+            </div>
+            <Routes>
+                <Route path={"/"} element={<TaskPage/>}/>
+                <Route path={"/add-task"} element={<Form/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
