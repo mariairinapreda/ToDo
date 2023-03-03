74eda485834053ad11df1449d361dbdd65a68312
mariairinapreda <mariairina.predaeu@gmail.com>
2023-03-03 17:46:11 +0200

    updated cors configuration

--- server/src/main/java/com/example/server/config/CorsConfig.java
+++ server/src/main/java/com/example/server/config/CorsConfig.java
@@ -1,6 +1,8 @@
 package com.example.server.config;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,11 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${app.baseurl}")
+    private String BASE_URL;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins(BASE_URL).allowedHeaders("Access-Control-Allow-Origin");
     }
 
 
